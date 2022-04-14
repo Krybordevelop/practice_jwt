383b5f5145b3b16fb9cd26f49c47c777809c8d8b
kry-bor <krybor.work@gmail.com>
2022-04-14 22:38:42 +0300

    add uniq to user model

--- model/user.js
+++ model/user.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    first_name: {type: String, default:null},
+    first_name: {type: String, default:null, require:true, require:true},
     last_name: {type: String, default:null},
-    email:{type:String, unique:true},
-    password:{type:String},
-    token:{type:String}
+    email:{type:String, unique:true,require:true},
+    password:{type:String, require:true}
+    //token:{type:String}
 })
 
 module.exports = mongoose.model('user', userSchema)
