932b2328e4121b2d83abde74928a4a0bcc225569
kry-bor <krybor.work@gmail.com>
2022-04-15 02:30:19 +0300

    start register

--- app.js
+++ app.js
@@ -6,57 +6,59 @@ const User = require('./model/user')
 const jwt = require('jsonwebtoken')
 const app = express()
 app.use(express.json())
-const auth  = require('./middleware/auth')
+const auth = require('./middleware/auth')
 app.post('/reg', async (req, res) => {
-    try{
-    const { first_name, last_name, email, password } = req.body;
+    try {
+        const { first_name, last_name, email, password } = req.body;
 
-    if (!(email && password && first_name && last_name)) {
-        res.status(400).json({ message: "all input is required" })
-    }
-
-    const oldUser = await User.findOne({ email })
-
-    if (oldUser) {
-        res.status(409).json({ message: "User already exist" })
-    }
-
-    encryptedPassword = await bcrypt.hash(password, 10)
-
-    const user = await User.create({
-        first_name,
-        last_name,
-        email: email.toLowerCase(),
-        password: encryptedPassword
-    })
-
-    const token = jwt.sign(
-        { user_id: user._id, email },
-        process.env.TOKEN_KEY,
-        {
-            expiresIn: '2h'
+        if (!(email && password && first_name && last_name)) {
+            res.status(400).json({ message: "all input is required" })
         }
-    );
 
-    user.token = token;
+        const oldUser = await User.findOne({ email })
 
-    res.status(201).json(user)
-    }catch(err){
+        if (oldUser) {
+            res.status(409).json({ message: "User already exist" })
+        }
+
+        encryptedPassword = await bcrypt.hash(password, 10)
+
+        const user = await User.create({
+            first_name,
+            last_name,
+            email: email.toLowerCase(),
+            password: encryptedPassword
+        })
+
+        const token = jwt.sign(
+            { user_id: user._id, email },
+            process.env.TOKEN_KEY,
+            {
+                expiresIn: '2h'
+            }
+        );
+
+        user.token = token;
+
+        res.status(201).json(user)
+    } catch (err) {
         console.log(err)
     }
 })
 
 app.post('/login', async (req, res) => {
-    try{
-        const {email, password} = req.body;
+    try {
+        const { email, password } = req.body;
 
-        if(!(email && password)){
-            res.status(400).json({message:"input is requred"})
+        if (!email) {
+            res.status(400).json({ message: "please type valid e-mail" })
+        } else if (!password) {
+            res.status(400).json({ message: "password is requred" })
         }
-        const user = await User.findOne({email});
-        if(user &&(await bcrypt.compare(password,user.password))){
+        const user = await User.findOne({ email });
+        if (user && (await bcrypt.compare(password, user.password))) {
             const token = jwt.sign(
-                {user_id:user._id,email},
+                { user_id: user._id, email },
                 process.env.TOKEN_KEY,
                 {
                     expiresIn: "2h"
@@ -67,13 +69,27 @@ app.post('/login', async (req, res) => {
 
             res.status(200).json(user)
         }
-        res.status(400).json({'message':'invaild credentials'} )
-    }catch(err){
+        res.status(400).json({ 'message': 'invaild credentials' })
+    } catch (err) {
         console.log(err)
-    } 
+    }
 })
 
-app.post('/welcome', auth, (req,res)=>{
+app.post("/reg", async (req,res) =>{
+    const {firstName, lastName, email, password} = req.body
+    if (!(firstName && email && password)){
+       return res.send("some field is requred")
+    }
+    try{
+        const newUser = new User({firstName, lastName, email,password})
+        const user = await newUser.save()
+        return res.send(user)
+    }catch(err){
+        res.status(500).json({"message":"server error"} )
+    }
+})
+
+app.post('/welcome', auth, (req, res) => {
     console.log(req.header)
     res.status(200).send('Welcome!')
 })
