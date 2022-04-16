c5b348de0803697ddf7ae00591537539a46d263c
krybor <krybordevelop@gmail.com>
2022-04-16 05:14:07 +0300

    add express validator

--- app.js
+++ app.js
@@ -4,92 +4,92 @@ const bcrypt = require('bcryptjs/dist/bcrypt');
 const express = require('express')
 const User = require('./model/user')
 const jwt = require('jsonwebtoken')
+const { body, validationResult } = require('express-validator');
 const app = express()
 app.use(express.json())
 const auth = require('./middleware/auth')
-app.post('/reg', async (req, res) => {
-    try {
-        const { first_name, last_name, email, password } = req.body;
 
-        if (!(email && password && first_name && last_name)) {
-            res.status(400).json({ message: "all input is required" })
-        }
+app.post('/reg',
+    body('first_name').notEmpty(),
+    body('email').isEmail().notEmpty(),
+    body('password').isLength({ min: 5 }).notEmpty(),
+    async (req, res) => {
+        try {
+            const { first_name, last_name, email, password } = req.body;
+            const errors = validationResult(req)
 
-        const oldUser = await User.findOne({ email })
-
-        if (oldUser) {
-            res.status(409).json({ message: "User already exist" })
-        }
-
-        encryptedPassword = await bcrypt.hash(password, 10)
-
-        const user = await User.create({
-            first_name,
-            last_name,
-            email: email.toLowerCase(),
-            password: encryptedPassword
-        })
-
-        const token = jwt.sign(
-            { user_id: user._id, email },
-            process.env.TOKEN_KEY,
-            {
-                expiresIn: '2h'
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
             }
-        );
 
-        user.token = token;
+            const oldUser = await User.findOne({ email })
 
-        res.status(201).json(user)
-    } catch (err) {
-        console.log(err)
-    }
-})
+            if (oldUser) {
+                return res.status(409).json({ message: "User already exist" })
+            }
 
-app.post('/login', async (req, res) => {
-    try {
-        const { email, password } = req.body;
+            encryptedPassword = await bcrypt.hash(password, 10)
 
-        if (!email) {
-            res.status(400).json({ message: "please type valid e-mail" })
-        } else if (!password) {
-            res.status(400).json({ message: "password is requred" })
-        }
-        const user = await User.findOne({ email });
-        if (user && (await bcrypt.compare(password, user.password))) {
+            const user = await User.create({
+                first_name,
+                last_name,
+                email: email.toLowerCase(),
+                password: encryptedPassword
+            })
+
             const token = jwt.sign(
                 { user_id: user._id, email },
                 process.env.TOKEN_KEY,
                 {
-                    expiresIn: "2h"
+                    expiresIn: '8h'
                 }
             );
 
             user.token = token;
 
-            res.status(200).json(user)
+            return res.status(201).json(user)
+        } catch (err) {
+            console.log(err)
+            return res.status(500).json({ "message": "server error" })
         }
-        res.status(400).json({ 'message': 'invaild credentials' })
-    } catch (err) {
-        console.log(err)
-    }
-})
+    })
 
-app.post("/reg", async (req,res) =>{
-    const {firstName, lastName, email, password} = req.body
-    if (!(firstName && email && password)){
-       return res.send("some field is requred")
-    }
-    try{
-        const newUser = new User({firstName, lastName, email,password})
-        const user = await newUser.save()
-        return res.send(user)
-    }catch(err){
-        res.status(500).json({"message":"server error"} )
-    }
-})
+app.post('/login',
+    body('email').isEmail().notEmpty(),
+    body('password').isLength({ min: 5 }).notEmpty(),
 
-app.post('/welcome', auth, (req, res) => {
+    async (req, res) => {
+        try {
+            const { email, password } = req.body;
+
+            const errors = validationResult(req)
+
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+
+            const user = await User.findOne({ email });
+            if (user && (await bcrypt.compare(password, user.password))) {
+                const token = jwt.sign(
+                    { user_id: user._id, email },
+                    process.env.TOKEN_KEY,
+                    {
+                        expiresIn: "2h"
+                    }
+                );
+
+                user.token = token;
+                return res.status(200).json(token)
+            }
+            return res.status(400).json({ 'message': 'invaild credentials' })
+        } catch (err) {
+            console.log(err)
+            return res.status(500).json({ 'message': 'server error' })
+        }
+    })
+
+
+app.get('/welcome', auth, (req, res) => {
     console.log(req.header)
     res.status(200).send('Welcome!')
 })
--- middleware/auth.js
+++ middleware/auth.js
@@ -2,14 +2,15 @@ const jwt = require('jsonwebtoken')
 const config = process.env
 
 const verefyToken = (req,res,next) =>{
-    console.log(req.headers["x-access-token"])
-    const token =  req.body.token || req.query.token || req.headers["x-access-token"]
+    console.log(req.headers["authorization"])
+    const token =  req.body.token || req.query.token || req.headers["authorization"]
 
     if(!token){
         return res.status(403).json('a token is required for auth')
     }
     try{
-        const decoded = jwt.verify(token, config.TOKEN_KEY)
+        console.log(token.split(' ')[1])
+        const decoded = jwt.verify(token.split(' ')[1], config.TOKEN_KEY)
         req.user = decoded;
     }catch(err){
         return res.status(401).json("invalid Token")
